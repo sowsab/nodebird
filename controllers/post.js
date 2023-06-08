52c16681cdf06a6e2cce56c22622b6076e184f58
yang <sowsab@naver.com>
2023-06-08 11:22:51 +0900

    init

---
+++ controllers/post.js
@@ -0,0 +1,31 @@
+const { User, Post, Hashtag } = require('../models');
+
+exports.afterUploadImage = (req, res)=>{
+    console.log(req.file);
+    res.json({url:`/img/${req.file.filename}`});
+};
+
+exports.uploadPost = async (req, res, next)=>{
+    try{
+        const post = await Post.create({
+            content: req.body.content,
+            img: req.body.url,
+            UserId: req.user.id,
+        });
+        const hashtags = req.body.content.match(/#[^\s#]*/g); //해시태그를 정규표현식으로 추출
+        if(hashtags){
+            const result = await Promise.all(
+                hashtags.map(tag=>{
+                    return Hashtag.findOrCreate({
+                        where:{ title:tag.slice(1).toLowerCase() },
+                    })
+                }),
+            );
+            await post.addHashtags(result.map(r=>r[0]));
+        }
+        res.redirect('/');
+    }catch(err){
+        console.error(err);
+        next(err);
+    }
+};
---
+++ models/index.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize');
+const User = require('./user');
+const Post = require('./post');
+const Hashtag = require('./hashtag');
+
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config.json')[env];
+const db = {};
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+db.sequelize = sequelize;
+db.User = User;
+db.Post = Post;
+db.Hashtag = Hashtag;
+
+User.initiate(sequelize);
+Post.initiate(sequelize);
+Hashtag.initiate(sequelize);
+
+User.associate(db);
+Post.associate(db);
+Hashtag.associate(db);
+
+module.exports = db;
